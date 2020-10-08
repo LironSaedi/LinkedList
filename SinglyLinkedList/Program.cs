d8ca58683db624891ca089e82b3b003bbf2b6fa0
Liron Saedi <Liron.Saedi@GMR.local>
2020-10-07 18:15:44 -0700

    I need to finish the add functions

--- SinglyLinkedList/Program.cs
+++ SinglyLinkedList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SinglyLinkedList
 {
@@ -9,8 +10,8 @@ namespace SinglyLinkedList
             public T Value;
             public Node<T> Next;
 
-            public Node(T value) { }
-            public Node(T value, Node<T> next) { }
+            public Node(T value) { this.Value = value; }
+            public Node(T value, Node<T> next) { this.Value = value; }
         }
 
         class LinkedList<T>
@@ -23,13 +24,90 @@ namespace SinglyLinkedList
             {
                 if (Head == null)
                 {
-                    Node<T> node = new Node<T>(value);
-                        }
+                    Node<T> node = new Node<T>(Value);
+                    Head = node;
+                    Tail = Head;
+
+                    
+                }
+                else
+                {
+                    Node<T> nodeToInsert = new Node<T>(Value);
+                    nodeToInsert.Next = Head;
+                    Head.Next = nodeToInsert;
+
+                    Count++;
+                }
+            }
+
+            public void AddLast(T Value)
+            {
+                if (Head == null)
+                {
+                    Node<T> node = new Node<T>(Value);
+                    Head = node;
+                    Tail.Next = Head;
+                    Count++;
+                }
+                else
+                {
+                    Node<T> otherNode = new Node<T>(Value);
+                    Tail.Next = otherNode;
+                    Tail = Tail.Next;
+                    Count++;
+                }
+            }
+
+            public void AddBefore(Node<T> node, T Value)
+            {
+
+                
+                Node<T> current = Head;
+                for (int i = 0; i < Count; i++, current = current.Next)
+                {
+                    if (current.Next == node)
+                    {
+                        Node<T> otherNode = new Node<T>(Value);
+                        otherNode.Next = current;
+                        node.Next = otherNode;
+                    }
+                }
+
+            }
+
+            public void AddAfter(Node<T> node, T Value)
+            {
+                Node<T> current = Head;
+                for (int i = 0; i < Count; i++, current = current.Next)
+                {
+                    if (current == node)
+                    {
+                        Node<T> otherNode = new Node<T>(Value);
+                        otherNode.Next = current;
+                        current.Next = otherNode;
+                    }
+                }
             }
         }
         static void Main(string[] args)
         {
 
+
+            int input = 12;
+            LinkedList<int> list = new LinkedList<int>();
+
+
+            list.AddFirst(12);
+            ;
+
+            list.AddLast(input);
+
+            Node<int> testNode = new Node<int>(7);
+            list.AddBefore(testNode, input);
+
         }
+
+
     }
 }
+
